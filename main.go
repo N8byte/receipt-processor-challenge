91cd28c5d6ee48e6ad4fa007a8060a3fce537173
Nathan Waskiewicz <nathanjwask@gmail.com>
2024-12-12 23:57:43 -0500

    Add unit tests, create CalculatePoints() - need to implement

--- main.go
+++ main.go
@@ -11,27 +11,46 @@ import (
 	"github.com/google/uuid"
 )
 
-// Map of receipts keyed on uuid.
+// Map of receipts keyed on uuid
 var receiptsMap = make(map[uuid.UUID]api.Receipt)
 
+// Server implements ServerInterface in /api/api_server.go
 type Server struct{}
 
+// Endpoint for /receipts/process
 func (s *Server) PostReceiptsProcess(c *gin.Context) {
 	var newReceipt api.Receipt
 	var newUuid = uuid.New()
 
+	// Attempt to parse the receipt
 	if err := c.BindJSON(&newReceipt); err != nil {
 		log.Println(err.Error())
 		return
 	}
 
+	// Store the new receipt
 	receiptsMap[newUuid] = newReceipt
 
 	c.IndentedJSON(http.StatusCreated, gin.H{"id": newUuid})
 }
 
+// Endpoint for /receipts/{id}/points
 func (s *Server) GetReceiptsIdPoints(c *gin.Context, id string) {
+	parsedId, err := uuid.Parse(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
+	var receipt = receiptsMap[parsedId]
+	var points = CalculatePoints(receipt)
+	
+	c.IndentedJSON(http.StatusOK, gin.H{"points": points})
+}
+
+// Calculate the points for a given receipt
+func CalculatePoints(receipt api.Receipt) int64 {
+	return 1234
 }
 
 func main() {
